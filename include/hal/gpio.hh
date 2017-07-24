e803e33a578b3037a872d676881ea78d365c535b
Boris Vinogradov <no111u3@gmail.com>
2017-07-24 20:23:08 +0300

    Simplify and refactoring
    [+] Adding set_mode, {set/reset}_value to gpio structure
    [*] Remove unneded namespaces for gpio and rcc
    [*] Renaming rcc_t to rcc
    [*] Change runner proto to upper updates

--- include/hal/gpio.hh
+++ include/hal/gpio.hh
@@ -3,51 +3,64 @@
 #include <stdint.h>
 
 namespace hal {
-    namespace gpio {
-        enum struct pin_mode_t : uint32_t {
-            input = 0b00,
-            output = 0b01,
-            alt_func = 0b10,
-            analog = 0b11
-        };
+    enum struct pin_mode_t : uint32_t {
+        input = 0b00,
+        output = 0b01,
+        alt_func = 0b10,
+        analog = 0b11
+    };
 
-        struct pin_t {
-            const uint32_t number;
-            const pin_mode_t mode;
-        };
+    struct pin_t {
+        const uint32_t number;
+        const pin_mode_t mode;
+    };
 
-        constexpr uint32_t mode_of(const pin_t pin) {
-            return (static_cast<uint32_t>(pin.mode) << (pin.number * 2));
-        }
-
-        template <typename Pin, typename ...Pins>
-        constexpr uint32_t mode_of(Pin pin, Pins ...pins) {
-            return mode_of(pin) | mode_of(pins...);
-        }
-
-        constexpr uint32_t value_of(const pin_t pin) {
-            return (1 << pin.number);
-        }
-
-        template <typename Pin, typename ...Pins>
-        constexpr uint32_t value_of(Pin pin, Pins ...pins) {
-            return value_of(pin) | value_of(pins...);
-        }
-
-        struct gpio_t {
-            uint32_t mode;
-            uint32_t otyper;
-            uint32_t ospeedr;
-            uint32_t pupdr;
-            uint32_t idr; 
-            uint32_t odr; 
-            uint32_t bsrr;
-            uint32_t lckr;
-            uint32_t afr[2];
-            uint32_t brr;
-        };
+    constexpr uint32_t mode_of(const pin_t pin) {
+        return (static_cast<uint32_t>(pin.mode) << (pin.number * 2));
     }
 
-    volatile gpio::gpio_t * const gpio_b = reinterpret_cast<gpio::gpio_t *>(0x40020400);
-}
+    template <typename Pin, typename ...Pins>
+    constexpr uint32_t mode_of(Pin pin, Pins ...pins) {
+        return mode_of(pin) | mode_of(pins...);
+    }
+
+    constexpr uint32_t value_of(const pin_t pin) {
+        return (1 << pin.number);
+    }
+
+    template <typename Pin, typename ...Pins>
+    constexpr uint32_t value_of(Pin pin, Pins ...pins) {
+        return value_of(pin) | value_of(pins...);
+    }
+
+    struct gpio_t {
+        uint32_t mode;
+        uint32_t otyper;
+        uint32_t ospeedr;
+        uint32_t pupdr;
+        uint32_t idr; 
+        uint32_t odr; 
+        uint32_t bsrr;
+        uint32_t lckr;
+        uint32_t afr[2];
+        uint32_t brr;
+
+        template <typename ...Pins>
+        void set_mode(Pins ...pins) volatile {
+            mode |= mode_of(pins...);
+        }
+
+        template <typename ...Pins>
+        void set_value(Pins ...pins) volatile {
+            bsrr = value_of(pins...);
+        }
+
+        template <typename ...Pins>
+        void reset_value(Pins ...pins) volatile {
+            bsrr = value_of(pins...) << 16;
+        }
+    };
+
+    volatile gpio_t * const gpio_b = reinterpret_cast<gpio_t *>(0x40020400);
+} // namespace Hal
 #endif // GPIO_HH
--- src/runner.cc
+++ src/runner.cc
@@ -4,21 +4,24 @@
 #include "hal/rcc.hh"
 
 void runner::run() {
-    const hal::gpio::pin_mode_t current_mode{hal::gpio::pin_mode_t::output};
-    const hal::gpio::pin_t ld3_green{7, current_mode};
-    const hal::gpio::pin_t ld4_blue{6, current_mode};
+    const hal::pin_mode_t current_mode{hal::pin_mode_t::output};
+    const hal::pin_t ld3_green{7, current_mode};
+    const hal::pin_t ld4_blue{6, current_mode};
     const uint32_t all_leds = value_of(ld3_green, ld4_blue);
     // enable led port
-    hal::rcc_r->ahb_enable.gpiob = 1;
+    hal::rcc->ahb_enable.gpiob = 1;
 
     // set led pins to output state
-    hal::gpio_b->mode |= mode_of(
+    hal::gpio_b->set_mode(
         ld3_green,
         ld4_blue
     );
 
     // enable leds
-    hal::gpio_b->odr |= all_leds;
+    hal::gpio_b->set_value(
+        ld3_green,
+        ld4_blue
+    );
 
     while (true) {
         volatile int cnt = 1 * 100 * 1000;

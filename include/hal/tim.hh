c5d737f73a3f5f8c97694102114d50015b60b1f8
Boris Vinogradov <no111u3@gmail.com>
2017-08-13 11:00:00 +0300

    Update hal/tim
    [+] extend top half for counter/compares (for 32bit tim5)
    [+] pointers to tim5,6,7,9,10,11

--- include/hal/tim.hh
+++ include/hal/tim.hh
@@ -154,26 +154,36 @@ namespace hal {
         uint16_t psc;
         uint16_t _reserved10;
         uint16_t arr;
-        uint16_t _reserved11;
+        uint16_t arr_ext;
         uint32_t _reserved12;
         uint16_t ccr1;
-        uint16_t _reserved13;
+        uint16_t ccr1_ext;
         uint16_t ccr2;
-        uint16_t _reserved14;
+        uint16_t ccr2_ext;
         uint16_t ccr3;
-        uint16_t _reserved15;
+        uint16_t ccr3_ext;
         uint16_t ccr4;
-        uint16_t _reserved16;
+        uint16_t ccr4_ext;
         uint32_t _reserved17;
         tim_dma_control_t dma_control;
         uint16_t _reserved18;
         uint16_t dmar;
         uint16_t _reserved19;
+        uint16_t option;
+        uint16_t _reserved20;
     };
 
     volatile tim_t * const tim2 = reinterpret_cast<tim_t *>(0x40000000);
     volatile tim_t * const tim3 = reinterpret_cast<tim_t *>(0x40000400);
     volatile tim_t * const tim4 = reinterpret_cast<tim_t *>(0x40000800);
+    volatile tim_t * const tim5 = reinterpret_cast<tim_t *>(0x40000c00);
+
+    volatile tim_t * const tim6 = reinterpret_cast<tim_t *>(0x40001000);
+    volatile tim_t * const tim7 = reinterpret_cast<tim_t *>(0x40001400);
+
+    volatile tim_t * const tim9 = reinterpret_cast<tim_t *>(0x40010800);
+    volatile tim_t * const tim10 = reinterpret_cast<tim_t *>(0x40010c00);
+    volatile tim_t * const tim11 = reinterpret_cast<tim_t *>(0x40011000);
 }
 
 #endif // TIM_HH

8a9f6d82c8cb88c6091e0a8810ab0b73efef1bf8
Boris Vinogradov <no111u3@gmail.com>
2017-09-09 19:41:35 +0300

    Update lib/out - remove static typing
    [U] Remove static typing and create consist function state

--- include/lib/out.hh
+++ include/lib/out.hh
@@ -4,11 +4,11 @@
 #include "lib/types.hh"
 
 namespace lib {
-    template <typename Outer, typename T = u32>
+    template <typename Outer, typename Type = u32>
     class out {
     public:
         using outer = Outer;
-        static constexpr auto t_bit_size = sizeof(T) * 8;
+        static constexpr auto t_bit_size = sizeof(Type) * 8;
 
         enum struct base : u8 {
             bin = 2,
@@ -17,6 +17,17 @@ namespace lib {
             hex = 16,
             alpha = 36
         };
+
+        template <typename T>
+        struct to_base {
+            const base bs;
+            const T t;
+        };
+
+        template <typename T>
+        static to_base<T> make_base(const base bs, const T t) {
+            return to_base<T>{bs, t};
+        }
 
         static void send(const s8 symbol) {
             outer::send_w(symbol);
@@ -28,32 +39,42 @@ namespace lib {
             }
         }
 
-        static void send(const s16 number) {
-            send(static_cast<s32>(number));
+        static void send(const s16 number,
+            const base out_base = base::dec) {
+            send(static_cast<s32>(number), out_base);
         }
 
-        static void send(const s32 number) {
+        static void send(const s32 number,
+            const base out_base = base::dec) {
             if (number < 0) {
                 send('-');
-                send(static_cast<u32>(number * -1));
+                send(static_cast<u32>(number * -1), out_base);
             } else {
-                send(static_cast<u32>(number));
+                send(static_cast<u32>(number), out_base);
             }
         }
 
-        static void send(const u8 number) {
-            send(static_cast<u32>(number));
+        static void send(const u8 number,
+            const base out_base = base::dec) {
+            send(static_cast<u32>(number), out_base);
         }
 
-        static void send(const u16 number) {
-            send(static_cast<u32>(number));
+        static void send(const u16 number,
+            const base out_base = base::dec) {
+            send(static_cast<u32>(number), out_base);
         }
 
-        static void send(const u32 number) {
+        template <typename T>
+        static void send(const to_base<T> t) {
+            send(t.t, t.bs);
+        }
+
+        static void send(const u32 number,
+            const base out_base = base::dec) {
             s8 output_buff[t_bit_size + 1]; // number of bits + '\0'
 
             const s8 *pout =
-                convert(output_buff + t_bit_size, number);
+                convert(output_buff + t_bit_size, number, out_base);
 
             send(pout);
         }
@@ -63,14 +84,16 @@ namespace lib {
             send(out);
             send(outs...);
         }
-
     private:
-        static const s8 * convert(s8 *output_buff, u32 number) {
+        static const s8 * convert(
+            s8 *output_buff,
+            u32 number,
+            const base out_base) {
             *output_buff-- = '\0';
             do {
                 s8 dig = number % static_cast<u8>(out_base);
                 if (dig > 9)
-                    dig += 0x27; // change to alpha symbols
+                    dig += 0x27;
                 *output_buff-- = dig + '0';
                 number /=
                     static_cast<u8>(out_base);
@@ -78,12 +101,7 @@ namespace lib {
 
             return output_buff + 1;
         }
-
-        static base out_base;
     };
-
-    template <typename Outer, typename T>
-    typename out<Outer, T>::base out<Outer, T>::out_base = base::dec;
 }
 
 #endif // OUT_HH

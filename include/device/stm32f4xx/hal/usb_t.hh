53979a8a48f7a62152404f89a39955cbdc25ffe2
Boris Vinogradov <no111u3@gmail.com>
2017-10-09 11:49:14 +0300

    Update hal/usb - adding hs support for stm32f4xx
    [U] specific hal/usb_t constanst and types usb hs for stm32f4xx family

--- include/device/stm32f4xx/hal/usb_t.hh
+++ include/device/stm32f4xx/hal/usb_t.hh
@@ -26,6 +26,19 @@ namespace hal {
             using otg_int_dbcdne = lib::regbit<19>;
 
             using ahb_cfg_gintmsk = lib::regbit<0>;
+            enum struct ahb_cfg_hbstlen_t : lib::u8 {
+                single = 0b0000,
+                incr = 0b0001,
+                incr4 = 0b0011,
+                incr8 = 0b0101,
+                incr16 = 0b0111
+            };
+            using ahb_cfg_hbstlen =
+                lib::regbit<1,
+                    ahb_cfg_hbstlen_t,
+                    4,
+                    ahb_cfg_hbstlen_t::single>;
+            using dmaen = lib::regbit<5>;
             using ahb_cfg_txfelvl = lib::regbit<7>;
             using ahb_cfg_ptxfelvl = lib::regbit<8>;
 
@@ -34,6 +47,16 @@ namespace hal {
             using usb_cfg_srpcat = lib::regbit<8>;
             using usb_cfg_hnpcat = lib::regbit<9>;
             using usb_cfg_trdt = lib::regbit8<10, 4>;
+            using usb_cfg_phylpcs = lib::regbit<15>;
+            using usb_cfg_ulpifsls = lib::regbit<17>;
+            using usb_cfg_ulpiar = lib::regbit<18>;
+            using usb_cfg_ulpicsm = lib::regbit<19>;
+            using usb_cfg_ulpievbusd = lib::regbit<20>;
+            using usb_cfg_ulpievbusi = lib::regbit<21>;
+            using usb_cfg_tsdps = lib::regbit<22>;
+            using usb_cfg_pcci = lib::regbit<23>;
+            using usb_cfg_ptci = lib::regbit<24>;
+            using usb_cfg_ulpiipd = lib::regbit<25>;
             using usb_cfg_fhmod = lib::regbit<29>;
             using usb_cfg_fdmod = lib::regbit<30>;
             using usb_cfg_ctxpkt = lib::regbit<31>;
@@ -44,6 +67,7 @@ namespace hal {
             using rst_ctl_rxfflsh = lib::regbit<4>;
             using rst_ctl_txfflsh = lib::regbit<5>;
             using rst_ctl_txfnum = lib::regbit8<6, 5>;
+            using rst_ctl_dmareq = lib::regbit<30>;
             using rst_ctl_ahbidl = lib::regbit<31>;
 
             using int_cmod = lib::regbit<0>;
@@ -64,6 +88,7 @@ namespace hal {
             using int_oepint = lib::regbit<19>;
             using int_iisoixfr = lib::regbit<20>;
             using int_ipxfr = lib::regbit<21>;
+            using int_datafsusp = lib::regbit<22>;
             using int_hprtint = lib::regbit<24>;
             using int_hcint = lib::regbit<25>;
             using int_ptxfe = lib::regbit<26>;
@@ -101,6 +126,7 @@ namespace hal {
                     rx_sts_pktsts_t,
                     4,
                     rx_sts_pktsts_t::in_data_recv>;
+            using rx_sts_frnum = lib::regbit8<21, 4>;
 
             using rx_fsiz_rxfd = lib::regbit16<0, 16, 16>;
 
@@ -113,7 +139,18 @@ namespace hal {
             using hnptx_sts_nptqxsav = lib::regbit16<16, 8>;
             using hnptx_sts_nptxqtop = lib::regbit16<24, 7>;
 
+            using i2cctl_rwdata = lib::regbit8<0, 8>;
+            using i2cctl_regaddr = lib::regbit8<8, 8>;
+            using i2cctl_addr = lib::regbit8<16, 7>;
+            using i2cctl_i2cen = lib::regbit<23>;
+            using i2cctl_ack = lib::regbit<24>;
+            using i2cctl_i2cdevaddr = lib::regbit8<26, 2>;
+            using i2cctl_i2cdatase0 = lib::regbit<28>;
+            using i2cctl_rw = lib::regbit<30>;
+            using i2cctl_bsydne = lib::regbit<31>;
+
             using ccfg_pwrdwn = lib::regbit<16>;
+            using ccfg_i2cpaden = lib::regbit<17>;
             using ccfg_vbusasen = lib::regbit<18>;
             using ccfg_vbusbsen = lib::regbit<19>;
             using ccfg_sofouten = lib::regbit<20>;
@@ -124,16 +161,6 @@ namespace hal {
 
             using dieptxf_ineptxsa = lib::regbit16<0, 16>;
             using dieptxf_ineptxfd = lib::regbit16<16, 16>;
-
-            using int_xfrc = lib::regbit<0>;
-            using int_chh = lib::regbit<1>;
-            using int_stall = lib::regbit<3>;
-            using int_nak = lib::regbit<4>;
-            using int_ack = lib::regbit<5>;
-            using int_txerr = lib::regbit<7>;
-            using int_bberr = lib::regbit<8>;
-            using int_frmod = lib::regbit<9>;
-            using int_dterr = lib::regbit<10>;
         }
 
         namespace host_mode {
@@ -218,6 +245,34 @@ namespace hal {
             using cha_chdis = lib::regbit<30>;
             using cha_chena = lib::regbit<31>;
 
+            using plt_prtaddr = lib::regbit8<0, 7>;
+            using plt_hubaddr = lib::regbit8<7, 7>;
+            enum struct plt_xactpos_t : lib::u8 {
+                mid = 0b00,
+                end = 0b01,
+                begin = 0b10,
+                all = 0b11
+            };
+            using plt_xactpos =
+                lib::regbit<14,
+                    plt_xactpos_t,
+                    2,
+                    plt_xactpos_t::mid>;
+            using plt_complsplt = lib::regbit<16>;
+            using plt_spliten = lib::regbit<31>;
+
+            using int_xfrc = lib::regbit<0>;
+            using int_chh = lib::regbit<1>;
+            using int_ahberr = lib::regbit<2>;
+            using int_stall = lib::regbit<3>;
+            using int_nak = lib::regbit<4>;
+            using int_ack = lib::regbit<5>;
+            using int_nyet = lib::regbit<6>;
+            using int_txerr = lib::regbit<7>;
+            using int_bberr = lib::regbit<8>;
+            using int_frmod = lib::regbit<9>;
+            using int_dterr = lib::regbit<10>;
+
             using tsiz_xfrsiz = lib::regbit32<0, 19>;
             using tsiz_pkcnt = lib::regbit16<19, 10>;
             enum struct tsiz_dpid_t : lib::u8 {
@@ -290,11 +345,16 @@ namespace hal {
             using iepmsk_ittxfemsk = lib::regbit<4>;
             using iepmsk_inepnmm = lib::regbit<5>;
             using iepmsk_inepnem = lib::regbit<6>;
+            using iepmsk_txfurm = lib::regbit<8>;
+            using iepmsk_bim = lib::regbit<9>;
 
             using oepmsk_xfrcm = lib::regbit<0>;
             using oepmsk_epdm = lib::regbit<1>;
             using oepmsk_stupm = lib::regbit<3>;
             using oepmsk_otepdm = lib::regbit<4>;
+            using oepmsk_b2bstup = lib::regbit<6>;
+            using oepmsk_opem = lib::regbit<8>;
+            using oepmsk_boim = lib::regbit<9>;
 
             using aint_iepint = lib::regbit16<0, 16>;
             using aint_oepint = lib::regbit16<16, 16>;
@@ -303,7 +363,36 @@ namespace hal {
 
             using vbuspulse_dvbusp = lib::regbit16<0, 12>;
 
+            using thrctl_nonisothren = lib::regbit<0>;
+            using thrctl_isothren = lib::regbit<1>;
+            using thrctl_txthrlen = lib::regbit16<2, 9>;
+            using thrctl_rxthren = lib::regbit<16>;
+            using thrctl_rxthrlen = lib::regbit16<17, 9>;
+            using thrctl_arpen = lib::regbit<27>;
+
             using iepempmsk_ineptxfem = lib::regbit16<0, 16>;
+
+            using eachint_iep1int = lib::regbit<1>;
+            using eachint_oep1int = lib::regbit<17>;
+
+            using inep1msk_xfrcm = lib::regbit<0>;
+            using inep1msk_epdm = lib::regbit<1>;
+            using inep1msk_tom = lib::regbit<3>;
+            using inep1msk_ittxfemsk = lib::regbit<4>;
+            using inep1msk_inepnmm = lib::regbit<5>;
+            using inep1msk_inepnem = lib::regbit<6>;
+            using inep1msk_txfurm = lib::regbit<8>;
+            using inep1msk_bim = lib::regbit<9>;
+            using inep1msk_nakm = lib::regbit<13>;
+
+            using outep1msk_xfrcm = lib::regbit<0>;
+            using outep1msk_epdm = lib::regbit<1>;
+            using outep1msk_ahberrm = lib::regbit<2>;
+            using outep1msk_opem = lib::regbit<8>;
+            using outep1msk_bim = lib::regbit<9>;
+            using outep1msk_berrm = lib::regbit<12>;
+            using outep1msk_nakm = lib::regbit<13>;
+            using outep1msk_nyetm = lib::regbit<14>;
         }
 
         namespace iep {
